bd1e1897114db0dddfa4def44b52ba07b3f7daea
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-21 16:26:03 +0630

    update(article): remove commented code from search service

--- src/features/articles/services/article-search.service.ts
+++ src/features/articles/services/article-search.service.ts
@@ -1,55 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
-import { ConfigSearch } from '../config/config.search';
 import { ArticleEntity } from '../entities/article.entity';
 import { IArticleSearchResult } from '../interfaces/articleSearch.interface';
-
-// @Injectable()
-// export class ArticleSearchService {
-//   _index = 'articles';
-
-//   constructor(private readonly elasticsearchService: ElasticsearchService) {}
-
-//   async indexArticle(article: ArticleEntity) {
-//     return this.elasticsearchService.index({
-//       index: this._index,
-//       body: {
-//         id: article.id,
-//         title: article.title,
-//         content: article.content,
-//         authorId: article.author.id,
-//       },
-//     });
-//   }
-
-//   async search(text: string) {
-//     const { hits } =
-//       await this.elasticsearchService.search<IArticleSearchResult>({
-//         index: this._index,
-//         body: {
-//           query: {
-//             multi_match: {
-//               query: text,
-//               fields: ['title', 'content'],
-//             },
-//           },
-//         },
-//       });
-//     const _hits = hits.hits;
-//     return _hits.map((item) => item._source);
-//   }
-// }
-
 @Injectable()
-export class ArticleSearchService extends ElasticsearchService {
+export class ArticleSearchService {
   _index = 'articles';
-
-  constructor() {
-    super(ConfigSearch.searchConfig(process.env.ELASTIC_SEARCH_URL));
-  }
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
 
   async indexArticle(article: ArticleEntity) {
-    return this.index({
+    return this.elasticsearchService.index({
       index: this._index,
       body: {
         id: article.id,
@@ -61,17 +20,18 @@ export class ArticleSearchService extends ElasticsearchService {
   }
 
   async searchArticles(text: string) {
-    const { hits } = await this.search<IArticleSearchResult>({
-      index: this._index,
-      body: {
-        query: {
-          multi_match: {
-            query: text,
-            fields: ['title', 'content'],
+    const { hits } =
+      await this.elasticsearchService.search<IArticleSearchResult>({
+        index: this._index,
+        body: {
+          query: {
+            multi_match: {
+              query: text,
+              fields: ['title', 'content'],
+            },
           },
         },
-      },
-    });
+      });
     const _hits = hits.hits;
     return _hits.map((item) => item._source);
   }
