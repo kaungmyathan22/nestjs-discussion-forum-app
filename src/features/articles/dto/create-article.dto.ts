d761fcd4d45e28cc9876a82651d6ba345fc2a615
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-26 14:17:18 +0630

    update(): create article dto added

--- src/features/articles/dto/create-article.dto.ts
+++ src/features/articles/dto/create-article.dto.ts
@@ -1,8 +1,21 @@
-import { IsString } from 'class-validator';
+import {
+  ArrayMinSize,
+  ArrayNotEmpty,
+  IsArray,
+  IsString,
+} from 'class-validator';
 
 export class CreateArticleDto {
   @IsString()
   title: string;
   @IsString()
   content: string;
+  @IsArray()
+  @IsString({
+    each: true,
+    message: 'Each element of the tags should be a string',
+  })
+  @ArrayNotEmpty({ message: 'tags should not be empty' })
+  @ArrayMinSize(1, { message: 'tags should have at least one element' })
+  tags: string[];
 }
