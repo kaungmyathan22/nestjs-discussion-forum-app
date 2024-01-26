7e72a59cb5c55e208c45c885b8b1ebaddb425a10
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-26 14:25:55 +0630

    update(articles): when creating aritcles also attach tags.

--- src/features/articles/services/articles.service.ts
+++ src/features/articles/services/articles.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateArticleDto } from '../dto/create-article.dto';
 import { UpdateArticleDto } from '../dto/update-article.dto';
 import { ArticleEntity } from '../entities/article.entity';
+import { TagEntity } from '../entities/tags.entity';
 import { ArticleSearchService } from './article-search.service';
 
 @Injectable()
@@ -12,15 +13,35 @@ export class ArticlesService {
   constructor(
     @InjectRepository(ArticleEntity)
     private readonly articleRepository: Repository<ArticleEntity>,
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
     private readonly articleSearchService: ArticleSearchService,
   ) {}
-  async create(user: UserEntity, createArticleDto: CreateArticleDto) {
-    const newArticle = this.articleRepository.create({
-      ...createArticleDto,
+  async create(user: UserEntity, payload: CreateArticleDto) {
+    const { title, content, tags } = payload;
+    let newArticle = this.articleRepository.create({
+      title,
+      content,
       author: user,
     });
-    await this.articleRepository.save(newArticle);
-    // await this.articleSearchService.indexArticle(newArticle);
+    newArticle = await this.articleRepository.save(newArticle);
+    await Promise.all(
+      tags.map(async (tag) => {
+        let tagEntity = await this.tagRepository.findOne({
+          where: { title: tag },
+          relations: ['articles'],
+        });
+        if (!tagEntity) {
+          tagEntity = await this.tagRepository.save(
+            this.tagRepository.create({ title: tag }),
+          );
+          tagEntity.articles = [];
+        }
+        tagEntity.articles.push(newArticle);
+        this.tagRepository.save(tagEntity);
+        return tagEntity;
+      }),
+    );
     return newArticle;
   }
 
