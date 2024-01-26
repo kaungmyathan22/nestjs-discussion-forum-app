c5fa3e41b844c815523d66417406491b5461ff74
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-26 16:00:30 +0630

    update(article): custom aritlce repo proider added

--- src/features/articles/articles.module.ts
+++ src/features/articles/articles.module.ts
@@ -1,15 +1,33 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  TypeOrmModule,
+  getDataSourceToken,
+  getRepositoryToken,
+} from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { SearchModule } from '../search/search.module';
 import { ArticlesController } from './articles.controller';
 import { ArticleEntity } from './entities/article.entity';
 import { TagEntity } from './entities/tags.entity';
+import { customArticleEntityRepositoryMethods } from './repositories/article.repository';
 import { ArticleSearchService } from './services/article-search.service';
 import { ArticlesService } from './services/articles.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ArticleEntity, TagEntity]), SearchModule],
   controllers: [ArticlesController],
-  providers: [ArticlesService, ArticleSearchService],
+  providers: [
+    ArticlesService,
+    ArticleSearchService,
+    {
+      provide: getRepositoryToken(ArticleEntity),
+      inject: [getDataSourceToken()],
+      useFactory(dataSource: DataSource) {
+        return dataSource
+          .getRepository(ArticleEntity)
+          .extend(customArticleEntityRepositoryMethods);
+      },
+    },
+  ],
 })
 export class ArticlesModule {}
