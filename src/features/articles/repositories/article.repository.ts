7c9be4eced9afc1601c6633b6004e93c196f1246
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-26 16:00:09 +0630

    feat(articles): article repo added

---
+++ src/features/articles/repositories/article.repository.ts
@@ -0,0 +1,34 @@
+import type { FindOptionsWhere, Repository } from 'typeorm';
+
+import {
+  PaginatedParamsDto,
+  PaginatedResponse,
+} from 'src/common/dto/pagination.dto';
+import { AbstractRepository } from 'src/database/abstract.repository';
+import { ArticleEntity } from '../entities/article.entity';
+
+export interface ArticleEntityRepository extends Repository<ArticleEntity> {
+  this: Repository<ArticleEntity>;
+
+  findAllWithPaginated(
+    queryParams?: PaginatedParamsDto,
+    filterQuery?: FindOptionsWhere<ArticleEntity>,
+  ): Promise<PaginatedResponse<ArticleEntity>>;
+}
+
+export const customArticleEntityRepositoryMethods: Pick<
+  ArticleEntityRepository,
+  'findAllWithPaginated'
+> = {
+  findAllWithPaginated(
+    this: Repository<ArticleEntity>,
+    queryParams?: PaginatedParamsDto,
+    filterQuery?: FindOptionsWhere<ArticleEntity>,
+  ) {
+    return AbstractRepository.findAllWithPaginated(
+      this,
+      queryParams,
+      filterQuery,
+    );
+  },
+};
