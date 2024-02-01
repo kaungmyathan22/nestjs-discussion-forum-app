96fbd8930853de3e7643afc8a505b1b86178978c
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-01 23:00:38 +0630

    feat(): swagger setup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/global-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,16 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   app.useGlobalFilters(new GlobalExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API Description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(conigService.get('PORT'));
 }
 bootstrap();
