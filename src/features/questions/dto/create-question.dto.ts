25b26c4cc19c545e7448abde8fa5bc2438d18ca3
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-01 23:01:06 +0630

    update(question): added sample request dto values

--- src/features/questions/dto/create-question.dto.ts
+++ src/features/questions/dto/create-question.dto.ts
@@ -1,1 +1,34 @@
-export class CreateQuestionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  ArrayMinSize,
+  ArrayNotEmpty,
+  IsArray,
+  IsString,
+} from 'class-validator';
+
+export class CreateQuestionDto {
+  @ApiProperty({
+    description: 'Title of the question',
+    example: 'How to center div in html.',
+  })
+  @IsString()
+  title: string;
+  @ApiProperty({
+    description: 'Content of the question may be markdown or wysiwyg content.',
+    example: 'Lorem ipsum. How can i center div in my html project?.',
+  })
+  @IsString()
+  content: string;
+  @ApiProperty({
+    example: ['html', 'css'],
+    description: 'Tags for question. Separated by comma in array.',
+  })
+  @IsArray()
+  @IsString({
+    each: true,
+    message: 'Each element of the tags should be a string',
+  })
+  @ArrayNotEmpty({ message: 'tags should not be empty' })
+  @ArrayMinSize(1, { message: 'tags should have at least one element' })
+  tags: string[];
+}
