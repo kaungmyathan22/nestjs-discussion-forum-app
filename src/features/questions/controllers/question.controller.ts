5fc198c495130adb24aa6b2c96e46fc2256ef847
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-01 23:01:29 +0630

    feat(question): swagger response types added

--- src/features/questions/controllers/question.controller.ts
+++ src/features/questions/controllers/question.controller.ts
@@ -3,18 +3,37 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import JwtAuthenticationGuard from 'src/features/authentication/guards/jwt.guard';
 import { CreateQuestionDto } from '../dto/create-question.dto';
 import { UpdateQuestionDto } from '../dto/update-question.dto';
 import { QuestionService } from '../services/question.service';
 
+@ApiTags('Questions')
 @Controller('api/v1/questions')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
+  @ApiOperation({ summary: 'Create a question' })
+  @ApiResponse({
+    status: 201,
+    description: 'Question has been successfully created.',
+    type: String,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  @ApiResponse({
+    status: 401,
+    description: 'Need to login to create question.',
+  })
+  @HttpCode(201)
+  @UseGuards(JwtAuthenticationGuard)
   @Post()
   create(@Body() createQuestionDto: CreateQuestionDto) {
     return this.questionService.create(createQuestionDto);
