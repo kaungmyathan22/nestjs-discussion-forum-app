d7a346080c44ad43b068b815b0f9c38c54527efe
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-02 16:34:40 +0630

    feat(answer): create answer reponse dto added

--- src/features/answers/dto/create-answer.dto.ts
+++ src/features/answers/dto/create-answer.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAnswerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateAnswerDto {
+  @ApiProperty({
+    description: 'Answer content for the question',
+    example: 'just use place-content: center to center a div.',
+  })
+  @IsString()
+  content: string;
+}
