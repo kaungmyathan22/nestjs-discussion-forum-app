9936c16863f1845b5bdd76f791eca9beb05627f5
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-02 15:20:55 +0630

    feat(vote): init commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { EmailModule } from './features/email/email.module';
 import { QuestionModule } from './features/questions/question.module';
 import { SearchModule } from './features/search/search.module';
 import { QueueModule } from './queue/queue.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
   imports: [
@@ -61,6 +62,7 @@ import { QueueModule } from './queue/queue.module';
     QuestionModule,
     SearchModule,
     ArticlesModule,
+    VotesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
