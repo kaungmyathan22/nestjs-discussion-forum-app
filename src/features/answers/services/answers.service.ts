a0b30d666dffcf517c107e8c64e2ece2e5786f9c
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-04 08:56:05 +0630

    feat(answers): answer service added

--- src/features/answers/services/answers.service.ts
+++ src/features/answers/services/answers.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PaginatedParamsDto } from 'src/common/dto/pagination.dto';
 import { QuestionEntity } from 'src/features/questions/entities/question.entity';
 import { UserEntity } from 'src/features/users/entities/user.entity';
-import { Repository } from 'typeorm';
 import { CreateAnswerDto } from '../dto/create-answer.dto';
 import { UpdateAnswerDto } from '../dto/update-answer.dto';
 import { AnswerEntity } from '../entities/answer.entity';
+import { AnswerRepository } from '../repositories/answer.repository';
 
 @Injectable()
 export class AnswersService {
   constructor(
     @InjectRepository(AnswerEntity)
-    private readonly answerRepository: Repository<AnswerEntity>,
+    private readonly answerRepository: AnswerRepository,
   ) {}
   async create(
     question: QuestionEntity,
@@ -27,19 +28,44 @@ export class AnswersService {
     return answerInstance;
   }
 
-  findAll() {
-    return `This action returns all answers`;
+  findAll(queryParams: PaginatedParamsDto) {
+    return this.answerRepository.findAllWithPaginated(queryParams);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} answer`;
+  async update(id: number, { content }: UpdateAnswerDto, user: UserEntity) {
+    const answer = await this.answerRepository.findOne({
+      where: { id, author: { id: user.id } },
+    });
+    if (!answer) {
+      throw new NotFoundException('Answer not found.');
+    }
+    answer.content = content;
+    await this.answerRepository.save(answer);
+    return {
+      message: 'Successfully updated the answer.',
+    };
   }
 
-  update(id: number, updateAnswerDto: UpdateAnswerDto) {
-    return `This action updates a #${id} answer`;
+  async remove(id: number, user: UserEntity) {
+    const result = await this.answerRepository.delete({
+      id,
+      author: { id: user.id },
+    });
+    if (result.affected < 1) {
+      throw new NotFoundException('answer with given id not found.');
+    }
+    return { message: 'Successfully deleted ansewwr.' };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} answer`;
+  answersByUser(queryParams: PaginatedParamsDto, user: UserEntity) {
+    return this.answerRepository.findAllWithPaginated(queryParams, {
+      author: { id: user.id },
+    });
+  }
+
+  answersByQuestion(queryParams: PaginatedParamsDto, question: QuestionEntity) {
+    return this.answerRepository.findAllWithPaginated(queryParams, {
+      question: { id: question.id },
+    });
   }
 }
